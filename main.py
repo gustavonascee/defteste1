c2113838d613334d8afe2fa57fcd6e6915b06cc3
gustavonascee <gustavonm04@gmail.com>
2026-05-30 11:54:46 -0300

    Adiciona API de consulta de preços

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+@app.get("/precos")
+def buscar_precos(data: str):
+    try:
+        resposta = requests.get(
+            "https://testedefensoriapr.pythonanywhere.com/precos"
+        )
+
+        dados = resposta.json()
+
+        return {
+            "data": data,
+            "dados": dados
+        }
+
+    except:
+        return {
+            "erro": "Não foi possível consultar a API externa, por favor tente novamente mais tarde"
+        }
